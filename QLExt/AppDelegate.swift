42416ccfc1d3e4a90f51542de0315b935d8f08ee
sbarex <sbarex@gmail.com>
2019-10-15 08:08:58 +0200

    Initial Commit

---
+++ QLExt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QLExt
+//
+//  Created by Simone Baldissini on 15/10/2019.
+//  Copyright © 2019 Simone Baldissini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QLExt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QLExt
+//
+//  Created by Simone Baldissini on 15/10/2019.
+//  Copyright © 2019 Simone Baldissini. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
